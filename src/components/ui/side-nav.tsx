3766a8ad3929fcba683bc55e986929aea6d95dac
Collin Stasiak <collinstasiak@gmail.com>
2024-07-02 12:15:08 -0400

    type error

--- src/components/ui/side-nav.tsx
+++ src/components/ui/side-nav.tsx
@@ -10,7 +10,7 @@ export default function SideNav({ userId }: { userId: string | null }) {
             <div className="mx-2 my-3">
                 <ul className="flex flex-col gap-2 mx-2 justify-start">
                     {userId ?
-                        <NavItem><Link href="/profile"><Button variant="ghost" className="p-2 justify-start w-full">Profile <ProfilePic id={userId} /></Button ></Link></NavItem>
+                        <NavItem><Link href="/profile"><Button variant="ghost" className="p-2 justify-start w-full">Profile <ProfilePic url={userId} /></Button ></Link></NavItem>
                         : ''}
                     <NavItem><Link href="/groups"><Button variant="ghost" className="p-2 justify-start w-full">Groups</Button></Link></NavItem>
                     <NavItem><Button variant="ghost" className="p-2 justify-start w-full">Placeholder</Button></NavItem>
